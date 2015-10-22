7214d1d2cc30098687cf502a929f0cd43a890307
Félix Gagnon-Grenier <fractalesque@gmail.com>
2015-10-22 14:19:36 -0400

    update for image loader

--- src/Manager.php
+++ src/Manager.php
@@ -5,11 +5,11 @@ namespace Imagize;
 class Manager implements Imaging {
 
     private $engine;
+    private $imageLoader;
 
-    public function __construct(Engine $engine) {
-
+    public function __construct(Engine $engine,ImageFileLoader $imageLoader) {
         $this->engine = $engine;
-
+        $this->imageLoader = $imageLoader;
     }
 
     public function save($path) {
