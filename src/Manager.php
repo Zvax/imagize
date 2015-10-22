c3c5f645bdf101cfa08a390be09971a7c77a5a4b
Félix Gagnon-Grenier <fractalesque@gmail.com>
2015-10-22 15:44:39 -0400

    removing useless engine

--- src/Manager.php
+++ src/Manager.php
@@ -2,26 +2,26 @@
 
 namespace Imagize;
 
+use Imagize\Exceptions\NotAnImageException;
+
 class Manager implements Imaging {
 
-    private $engine;
     private $imageLoader;
-
     private $image;
 
-    public function __construct(Engine $engine,ImageFileLoader $imageLoader) {
-        $this->engine = $engine;
+    public function __construct(ImageFileLoader $imageLoader) {
         $this->imageLoader = $imageLoader;
-    }
-
-    public function save($path) {
-        $this->engine->sauverVersPath($path);
     }
 
     public function load($fileName) {
         $content = $this->imageLoader->getImageFile($fileName);
-        $image = new Image($content);
-        return $image;
+        $this->image = new Image($content);
+        return $this->image;
+    }
+
+    public function save($path) {
+        if ($this->image === null) throw new NotAnImageException($path);
+        $this->image->sauverVersPath($path);
     }
 
 }
