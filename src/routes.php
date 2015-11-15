1a58596f1ce77279595f319fe8b45e65866dce3c
Félix Gagnon-Grenier <fractalesque@gmail.com>
2015-11-15 11:51:14 -0500

    fix order hiding subsequent routes

--- src/routes.php
+++ src/routes.php
@@ -2,9 +2,9 @@
 
 return [
 
-    ['GET', '/images/{filename:[a-zA-Z0-9_\/.]+}', ['Imagize\Imagize', 'serveOriginal']],
-    ['GET', '/images/{width:[0-9]+}/{filename:[a-zA-Z0-9_\/.]+}', ['Imagize\Imagize', 'serveResized']],
     ['GET', '/images/{width:[0-9]+}/{height:[0-9]+}/{filename:[a-zA-Z0-9_\/.]+}', ['Imagize\Imagize', 'serveResized']],
+    ['GET', '/images/{width:[0-9]+}/{filename:[a-zA-Z0-9_\/.]+}', ['Imagize\Imagize', 'serveResized']],
+    ['GET', '/images/{filename:[a-zA-Z0-9_\/.]+}', ['Imagize\Imagize', 'serveOriginal']],
 
     ['GET', '/', ['Imagize\Imagize', 'serveDirectory']],
     ['GET', '/{action}', ['Imagize\Imagize', 'serveDirectory']],
